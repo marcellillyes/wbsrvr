07307969432c80a1b0e86ffe231f122283f518dd
marcellillyes <47120552+marcellillyes@users.noreply.github.com>
2020-01-16 20:06:15 +0000

    Update users.js

--- routes/users.js
+++ routes/users.js
@@ -3,7 +3,10 @@ var router = express.Router();
 var {PythonShell} = require('python-shell');
 var WebSocket = require('ws');
 
+//creating websocketclient that is connected to server
+const ws = new WebSocket('ws://localhost:3000', )
 
+//Python shell options
 var options = {
   mode: 'json',
   pythonPath: 'C:\\Users\\illye\\AppData\\Local\\Programs\\Python\\Python38-32\\python.exe',
@@ -14,25 +17,21 @@ var options = {
 
 var pyshell = new PythonShell('processing.py', options);
 
-
-
+//Python shell event listener sends incoming json objects to server (IIR scaled up coefficients)
 pyshell.on('message', function (message) {
-  // received a message sent from the Python script (a simple "print" statement)
-  console.log(JSON.stringify(message));
+  ws.send(JSON.stringify(message),{}, function () {
+  });
 });
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
   res.render('ADCSettings');
-  console.log(2);
 });
 
 router.post("/", function (req, res, next) {
   console.log(req.body);
   pyshell.send(req.body);
-
   res.render('ADCSettings');
-
 });
 
 module.exports = router;
